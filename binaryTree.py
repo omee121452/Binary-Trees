e039fe5203f083ba8deee0c18a733421bc2f1696
omee121452 <omwins2@gmail.com>
2023-01-29 18:04:27 -0800

    Introduce CurrentParent Variabel that changes as you go through the binary Tree.

--- binaryTree.py
+++ binaryTree.py
@@ -8,10 +8,22 @@ class BinaryTree:
     def insert(self, Newnumber):
         if self.root == None:
             self.root = Node(Newnumber, None)
-        if self.root.number > Newnumber:
-            self.root.leftChild = Node(Newnumber, self.root)
-        if self.root.number < Newnumber:
-            self.root.rightChild = Node(Newnumber, self.root)
+        # if self.root.number > Newnumber:
+        #     self.root.leftChild = Node(Newnumber, self.root)
+        # if self.root.number < Newnumber:
+        #     self.root.rightChild = Node(Newnumber, self.root)
+        CurrentParent = self.root
+        #add a while loop for CurrentParent
+        if CurrentParent.number > Newnumber:
+            if CurrentParent.leftChild != None:
+                CurrentParent = CurrentParent.leftChild
+            else:
+                CurrentParent.leftChild = Node(Newnumber, CurrentParent)
+        if CurrentParent.number < Newnumber:
+            if CurrentParent.leftChild != None:
+                CurrentParent = CurrentParent.rightChild
+            else:
+                CurrentParent.rightChild = Node(Newnumber, CurrentParent)
 
     def __str__(self):
         pass
