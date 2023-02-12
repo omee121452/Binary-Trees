bc34e112f04e3855e8c1c78a702a717d2879b245
omee121452 <omwins2@gmail.com>
2023-02-12 14:10:30 -0800

    Added while loop logic to allow multiple nodes to be added

--- binaryTree.py
+++ binaryTree.py
@@ -6,24 +6,29 @@ class BinaryTree:
         self.root = None
 
     def insert(self, Newnumber):
+        NodeCreated = False
         if self.root == None:
             self.root = Node(Newnumber, None)
+            NodeCreated = True
         # if self.root.number > Newnumber:
         #     self.root.leftChild = Node(Newnumber, self.root)
         # if self.root.number < Newnumber:
         #     self.root.rightChild = Node(Newnumber, self.root)
         CurrentParent = self.root
-        #add a while loop for CurrentParent
-        if CurrentParent.number > Newnumber:
-            if CurrentParent.leftChild != None:
-                CurrentParent = CurrentParent.leftChild
-            else:
-                CurrentParent.leftChild = Node(Newnumber, CurrentParent)
-        if CurrentParent.number < Newnumber:
-            if CurrentParent.leftChild != None:
-                CurrentParent = CurrentParent.rightChild
-            else:
-                CurrentParent.rightChild = Node(Newnumber, CurrentParent)
+        # add a while loop for CurrentParent
+        while NodeCreated != True:
+            if Newnumber < CurrentParent.number:
+                if CurrentParent.leftChild != None:
+                    CurrentParent = CurrentParent.leftChild
+                else:
+                    CurrentParent.leftChild = Node(Newnumber, CurrentParent)
+                    NodeCreated = True
+            if Newnumber > CurrentParent.number:
+                if CurrentParent.rightChild != None:
+                    CurrentParent = CurrentParent.rightChild
+                else:
+                    CurrentParent.rightChild = Node(Newnumber, CurrentParent)
+                    NodeCreated = True
 
     def __str__(self):
         pass
@@ -34,3 +39,9 @@ bt1 = BinaryTree()
 print(bt1.root)
 bt1.insert(10)
 print(bt1.root)
+bt1.insert(8)
+bt1.insert(2)
+bt1.insert(80)
+bt1.insert(200)
+print(bt1.root.rightChild.rightChild)
+#print(bt1.root.leftChild.leftChild)
